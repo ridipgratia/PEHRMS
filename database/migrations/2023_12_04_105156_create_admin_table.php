6da2246cf77a000c304a238ef0bed0b9eea2abd3
ridipblog <memorytemp5@gmail.com>
2023-12-04 16:57:31 +0530

    Add Admin COntroller 04-12-23

---
+++ database/migrations/2023_12_04_105156_create_admin_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin', function (Blueprint $table) {
+            $table->id();
+            $table->string('admin_code');
+            $table->string('email');
+            $table->string('password');
+            $table->integer('role'); //1 for Admin / 2 for Employe
+            $table->integer('active_status')->default(1); // Default 1 for active / 0 inactive
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin');
+    }
+}
